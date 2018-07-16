1e90d8ed7968a00db297d288ffd038bedd80f4c8
Jay Shong <30782265+Fourleaftayback@users.noreply.github.com>
2018-07-16 17:01:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let urlWiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
-let searchResult = [];
-let j = 0;
-let n = 0;
+const urlWiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=";
+
+const global = {
+  j: 0,
+  n: 0,
+  searchResult: []
+}
 
 function jsonp(url, callback) {
   let myCallBack = 'jsonp_callback_' + Math.round(100000 * Math.random());
@@ -23,7 +26,7 @@ function searchUrl() {
     document.getElementById('search-result').innerHTML = "Please Enter Something";
   } else {
     let request = new jsonp(urlWiki + term + "&format=json", function(data) {
-      searchResult = data;
+      global.searchResult = data;
       displayResult();
     });
   }
@@ -31,19 +34,19 @@ function searchUrl() {
 
 function displayResult() {
   let text = "";
-  if (searchResult[1].length == 0) {
+  if (global.searchResult[1].length == 0) {
     text = "<h5>Sorry no results found</h5>";
   } else {
-    for (i in searchResult[1]) {
-      text += `<div><a href=\""${searchResult[3][j]}"\" target=\"_blank\"><h4>${searchResult[1][i]}</h4></a>`
-      j++;
-      text += `<p>${searchResult[2][n]}</p></div>`
-      n++;
+    for (i in global.searchResult[1]) {
+      text += `<div><a href=\""${global.searchResult[3][global.j]}"\" target=\"_blank\"><h4>${global.searchResult[1][i]}</h4></a>`
+      global.j++;
+      text += `<p>${global.searchResult[2][global.n]}</p></div>`
+      global.n++;
     }
   }
   document.getElementById('search-result').innerHTML = text;
-  j = 0;
-  n = 0;
+  global.j = 0;
+  global.n = 0;
 }
 
 function checkKey(e) {
